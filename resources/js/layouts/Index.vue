ad38bdbbfd8cf8eb35582994642e11d61f9cbb4a
Moin Iqbal <moin.iqbal@compnow.com.au>
2023-03-08 00:48:24 +1100

    New features: table search, pagination, email sorting and page option selector

--- resources/js/layouts/Index.vue
+++ resources/js/layouts/Index.vue
@@ -1,19 +1,16 @@
 <template>
-    <div class="container">
-        <vs-table
-            :sst="true"
-            @search="handleSearch"
-            @change-page="handleChangePage"
-            @sort="handleSort"
-            v-model="selected"
-            :total="totalItems"
-            max-items="3"
-            search
-            :data="users"
-            stripe
-        >
+    <div class="container p-4">
+        <vs-row><h3>Student Enrolment Page</h3></vs-row>
+        <vs-table search pagination :max-items="maxItems" :data="users" stripe>
             <template slot="header">
-                <h3>Users</h3>
+                <vs-select label="Page Options" v-model="maxItems">
+                    <vs-select-item
+                        :key="index"
+                        :value="item"
+                        :text="item"
+                        v-for="(item, index) in paginationOptions"
+                    />
+                </vs-select>
             </template>
             <template slot="thead">
                 <vs-th sort-key="name"> Name </vs-th>
@@ -30,21 +27,9 @@
                         {{ data[indextr].email }}
                     </vs-td>
 
-                    <!-- <li>COURSE: {{ enrolment.course.description }}</li> -->
                     <template slot="expand">
                         <div v-if="tr.enrolments.length > 0">
                             <enrolments-table :enrolments="tr.enrolments" />
-
-                            <!-- <ul v-for="enrolment in tr.enrolments" :key="enrolment.id">
-                                <li>
-                                    {{ enrolment.id }}
-                                    <div>
-                                        STATUS: {{ enrolment.completion_status }} <br />
-                                        STATUS: {{ enrolment.completion_status }} <br />
-                                        STATUS: {{ enrolment.completion_status }} <br />
-                                    </div>
-                                </li>
-                            </ul> -->
                         </div>
                         <div v-else>No enrolment data available</div>
                     </template>
@@ -64,42 +49,23 @@ export default {
     components: { EnrolmentsTable },
     data() {
         return {
-            selected: [],
-            totalItems: 10,
             users: [],
-            search: ''
+            defaultMaxItems: 10,
+            paginationOptions: ['10', '15', '20', '25']
         }
     },
     computed: {
-        filteredList() {
-            if (this.search) {
-                return this.users.filter((user) => {
-                    return user.first_name.toLowerCase().includes(this.search.toLowerCase())
-                })
+        maxItems: {
+            get() {
+                return this.defaultMaxItems
+            },
+            set(val) {
+                this.defaultMaxItems = val
             }
-
-            return this.users
-        }
-    },
-    methods: {
-        handleSearch(searching) {
-            this.search = searching
-
-            // let _print = `The user searched for: ${searching}\n`
-            // this.$refs.pre.appendChild(document.createTextNode(_print))
-        },
-        handleChangePage(page) {
-            let _print = `The user changed the page to: ${page}\n`
-            this.$refs.pre.appendChild(document.createTextNode(_print))
-        },
-        handleSort(key, active) {
-            let _print = `the user ordered: ${key} ${active}\n`
-            this.$refs.pre.appendChild(document.createTextNode(_print))
         }
     },
     async mounted() {
         var response = await this.$api.get('/users')
-        console.log(response.data)
         this.users = response.data
     }
 }
